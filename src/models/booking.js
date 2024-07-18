345d53740a1d29fe8507068967803c8b1996b281
favour <fackonu@st.ug.edu.gh>
2024-07-18 23:52:12 +0000

    Update booking model with status enums and IDs

--- src/models/booking.js
+++ src/models/booking.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
     bookingId: {type: String, required: true, unique: true},
-    date: {type: String, required: true},
-    status: {type: Boolean, required: true}
+    customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
+    serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
+    cleanerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Cleaner'}, // cleanerId will only be added when a cleaner accepts the booking
+    date: {type: Date, required: true},
+    bookingStatus: { 
+        type: String, 
+        enum: ['pending', 'accepted', 'declined', 'in progress', 'completed', 'pending payment', 'cancelled'], 
+        required: true,
+        default: 'pending' 
+    }
 });
 
 const Booking = mongoose.model('Booking', bookingSchema);
