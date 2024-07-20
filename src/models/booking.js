12797993d576c941aa9a2325aace29dcb3ac47d4
favour <fackonu@st.ug.edu.gh>
2024-07-20 13:18:44 +0000

    Update date attribute to default to current date

--- src/models/booking.js
+++ src/models/booking.js
@@ -5,7 +5,7 @@ const bookingSchema = new mongoose.Schema({
     customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
     serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
     cleanerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Cleaner'}, // cleanerId will only be added when a cleaner accepts the booking
-    date: {type: Date, required: true},
+    date: {type: Date, required: true, default: Date.now},
     bookingStatus: { 
         type: String, 
         enum: ['pending', 'accepted', 'declined', 'in progress', 'completed', 'pending payment', 'cancelled'], 
