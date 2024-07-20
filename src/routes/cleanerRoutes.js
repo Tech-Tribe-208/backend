50c33e08372342f64ec2671e37547f05f0f7b8e3
favour <fackonu@st.ug.edu.gh>
2024-07-20 13:52:22 +0000

    Add cleaner endpoint for updating booking status
    - the status of bookings can be updated by cleaners
    - bookings with cancelled or completed statuses cannot be updated

--- src/routes/cleanerRoutes.js
+++ src/routes/cleanerRoutes.js
@@ -45,4 +45,49 @@ cleanerRouter.get('/bookings', async (req, res) => {
     }
 })
 
+cleanerRouter.post('/bookings', async (req, res) => {
+    try{
+        console.log('we\'re in the try block');
+        const {bookingId, status} = req.body;
+        const booking = await Booking.findOne
+        ({bookingId});
+    }
+    catch(error){
+        res.status(500).json({responseCode: '101', responseMessage: 'fatal error'});
+        console.error(error);
+    }
+})
+
+cleanerRouter.patch('/bookings/:bookingId', async (req, res) => {
+    try{
+        console.log('we\'re in the try block');
+        const {bookingId} = req.params;
+        const {bookingStatus} = req.body;
+        const booking = await Booking.findOne({bookingId});
+        if(booking){
+            console.log('we\'ve found the booking');
+            const bookingStatuses = ['pending', 'accepted', 'declined', 'in progress', 'completed', 'pending payment', 'cancelled'];
+            if(!bookingStatuses.includes(bookingStatus)){
+                console.log('invalid booking status');
+                return res.status(400).json({responseCode: '400', responseMessage: 'Invalid booking status'});
+            }
+            if (['completed', 'cancelled'].includes(booking.bookingStatus)) {
+                console.log('booking status cannot be updated');
+                return res.status(400).json({ responseCode: '400', responseMessage: 'Booking status cannot be updated' });
+            }
+            booking.bookingStatus = bookingStatus;
+            await booking.save();
+            res.status(200).json({responseCode: '200', responseMessage: 'Booking status updated', responseData: booking});
+        }
+        else{
+            console.log('we didn\'t find the booking');
+            res.status(404).json({responseCode: '404', responseMessage: 'Booking not found'});
+        }
+    }
+    catch(error){
+        res.status(500).json({responseCode: '101', responseMessage: 'fatal error'});
+        console.error(error);
+    }
+})
+
 module.exports = cleanerRouter;
