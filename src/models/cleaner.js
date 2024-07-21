1dcd868e2e041fcd3901bc8f47e71d24a9a12ebb
favour <fackonu@st.ug.edu.gh>
2024-07-21 20:43:20 +0000

    Add rating attribute to cleaner model

--- src/models/cleaner.js
+++ src/models/cleaner.js
@@ -4,7 +4,8 @@ const cleanerSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   fullName: { type: String, required: true },
   password: { type: String, required: true, select: false },
-  email: { type: String, required: true, unique: true }
+  email: { type: String, required: true, unique: true },
+  rating: { type: Number, default: 5.0 },
 });
 
 const Cleaner = mongoose.model('Cleaner', cleanerSchema);
