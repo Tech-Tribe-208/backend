8e4307172bd3f4217e0f96f426177705712f53f2
favour <fackonu@st.ug.edu.gh>
2024-07-21 21:47:13 +0000

    Add number of ratings attribute to cleaner model

--- src/models/cleaner.js
+++ src/models/cleaner.js
@@ -6,6 +6,7 @@ const cleanerSchema = new mongoose.Schema({
   password: { type: String, required: true, select: false },
   email: { type: String, required: true, unique: true },
   rating: { type: Number, default: 5.0 },
+  numberOfRatings: { type: Number, default: 0 },
 });
 
 const Cleaner = mongoose.model('Cleaner', cleanerSchema);
