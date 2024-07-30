d86745892e8b1aadcda25fce6bab7d6f5dd8d377
favour <fackonu@st.ug.edu.gh>
2024-07-30 21:51:31 +0000

    Use mongoose

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,12 @@
-var mongoClient = require("mongodb").MongoClient;
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const connectionString = process.env.MONGODB_CONNECTION_STRING;
 
 const connectDB = async () =>{
     try{
-        mongoClient.connect(connectionString, function (err, db) {
-        db.close();
-        });
-        console.log("Successfully connected to MongoDB.");
+        await mongoose.connect(connectionString);
+        console.log("Successfully connected to MongoDB");
     }
     catch(err){
         console.error("Error connecting to MongoDB", err);
