3755e392f662ba150c73a89218ea006aab77e315
favour <fackonu@st.ug.edu.gh>
2024-07-31 19:56:50 +0000

    Use bcryptjs instead of bcrypt

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const adminRouter = express.Router();
 
